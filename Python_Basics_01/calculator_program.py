7236600270a0bd3f3eadb5875e8f76bc59f80ff7
Nouran Nasser <93429068+nourannaser9345@users.noreply.github.com>
2026-09-06 15:19:32 +0300

    Update calculator_program.py

--- Python_Basics_01/calculator_program.py
+++ Python_Basics_01/calculator_program.py
@@ -1,14 +1,14 @@
-Number1 = int (input("Please Enter your Calculs Number : ").strip())
-Number2 = int(input("Please Enter your Calculs Number : ").strip())
-Selection = input("Please Enter your math Opertion (+ , - , * , //) : ").strip()
+Number1 = float(input("Please Enter your Calculs Number : ").strip())
+Number2 = float(input("Please Enter your Calculs Number : ").strip())
+Selection = input("Please Enter your math Opertion (+ , - , * , /) : ").strip()
 
 if Selection == '+' :
-    print(Number1 + Number2 )
+    print(round(Number1 + Number2 , 3))
 elif Selection == '-':
-    print(Number1 - Number2)
+    print(round(Number1 - Number2 , 3))
 elif Selection == '*':
-    print(Number1 * Number2)
-elif Selection == '//':
-   print(Number1 // Number2)
+    print(round(Number1 * Number2 , 3))
+elif Selection == '/':
+   print(round(Number1 / Number2 , 3))
 else:
     print("Invalid Operator")
