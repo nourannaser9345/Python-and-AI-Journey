fff64bf10772c2f8ab01cd4e15332b2c4d7a1f99
Nouran Nasser <93429068+nourannaser9345@users.noreply.github.com>
2026-09-06 14:21:16 +0300

    Update calculator_program.py

--- Python_Basics_01/calculator_program.py
+++ Python_Basics_01/calculator_program.py
@@ -1,1 +1,14 @@
+Number1 = int (input("Please Enter your Calculs Number : ").strip())
+Number2 = int(input("Please Enter your Calculs Number : ").strip())
+Selection = input("Please Enter your math Opertion (+ , - , * , //) : ").strip()
 
+if Selection == '+' :
+    print(Number1 + Number2 )
+elif Selection == '-':
+    print(Number1 - Number2)
+elif Selection == '*':
+    print(Number1 * Number2)
+elif Selection == '//':
+   print(Number1 // Number2)
+else:
+    print("Invalid Operator")
